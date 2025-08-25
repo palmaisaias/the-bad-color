2b0f57cd383a2043550f18169eb7680568b0d0f6
palmaisaias <palmaisaias@yahoo.com>
2025-08-25 08:00:06 -0700

    react import fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 // --- Quick setup notes -------------------------------------------------------
 // 1) Drop your PNGs (image1.png, image2.png, ...) and your MP4 into the same
